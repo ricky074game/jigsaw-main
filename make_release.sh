ccf54ff4b66734146b22b2930bfd60c34ae946b8
ricky074game <ricky.sambataro@gmail.com>
2025-02-19 16:47:55 -0500

    Fix stuff

--- make_release.sh
+++ make_release.sh
@@ -2,8 +2,11 @@
 # shellcheck disable=SC2164
 cd "${0%/*}"
 echo "Building release..."
+sudo apt-get update && sudo apt-get install -y libudev-dev
+
 cargo install trunk
 trunk build --release
+rustup target add wasm32-unknown-unknown
 cargo build --release --bin server
 rm -f jigsaw.zip
 zip jigsaw.zip -j target/release/server run.sh
