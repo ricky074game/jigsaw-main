e957a6d283fec1f1c1ebb9a5ac9b0ed1bd9c8b4b
ricky074game <ricky.sambataro@gmail.com>
2025-02-19 16:37:30 -0500

    Work

--- make_release.sh
+++ make_release.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2164
 cd "${0%/*}"
+echo "Building release..."
+cargo install trunk
 trunk build --release
 cargo build --release --bin server
 rm -f jigsaw.zip
 zip jigsaw.zip -j target/release/server run.sh
 zip jigsaw.zip -r dist/
+unzip jigsaw.zip -d /tmp/jigsaw
